d68a9d8be4e27f2a9c19813d4791b7f3ae370b3d
kurawa-20 <sepertiberjalan@gmail.com>
2026-03-03 11:48:53 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'jadwal-puasa-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetching
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
